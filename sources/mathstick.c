f37c7c8d893b1391d35acbf1bc7e3123e49c45d2
Florian FREVOL <florianfre@localhost.localdomain>
2018-02-11 17:15:54 +0100

    [error output]

--- sources/mathstick.c
+++ sources/mathstick.c
@@ -28,13 +28,13 @@ int check_enought_2(char *str, char **tabl, s_t *s)
 int errors(int argc, char **argv, s_t *s)
 {
 	if (argc < 3) {
-		my_putstr("argument invalid\n");
+		my_error("argument invalid\n");
 		return (1);
 	}
 	s->nbr_lign = get_nbr(argv[1]);
 	s->nbr_max_stick = get_nbr(argv[2]);
 	if (s->nbr_lign > 99 || s->nbr_lign == 0) {
-		my_putstr("numbers of sticks invalid\n");
+		my_error("numbers of sticks invalid\n");
 		return (1);
 	}
 	s->ia_or_you = 1;
